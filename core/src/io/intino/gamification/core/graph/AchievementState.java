bd7e9aac4cd66ede023998f9a9fd48655d0ef02b
RaulLozanoPonce <raul.lozano.ponce@hotmail.com>
2021-06-01 14:59:50 +0100

    Fix

---
+++ core/src/io/intino/gamification/core/graph/AchievementState.java
@@ -0,0 +1,17 @@
+package io.intino.gamification.core.graph;
+
+public class AchievementState extends AbstractAchievementChecked {
+
+	public AchievementState(io.intino.magritte.framework.Node node) {
+		super(node);
+	}
+
+	public io.intino.gamification.core.box.events.enumerates.AchievementState status() {
+		return io.intino.gamification.core.box.events.enumerates.AchievementState.valueOf(statusName);
+	}
+
+	public AchievementState status(io.intino.gamification.core.box.events.enumerates.AchievementState status) {
+		statusName(status.name());
+		return this;
+	}
+}

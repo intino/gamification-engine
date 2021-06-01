7bf21ec16fc47d7ea48e75651f744c75b42b9cd5
RaulLozanoPonce <rlozano@monentia.es>
2021-06-01 10:34:15 +0100

    Merge

--- core/src/io/intino/gamification/core/box/mounter/EntityMounter.java
+++ core/src/io/intino/gamification/core/box/mounter/EntityMounter.java
@@ -1,9 +1,12 @@
 package io.intino.gamification.core.box.mounter;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import io.intino.gamification.core.box.CoreBox;
 import io.intino.gamification.core.box.events.*;
 import io.intino.gamification.core.graph.Entity;
-import io.intino.magritte.framework.Layer;
+
+import java.util.Map;
 
 public class EntityMounter extends Mounter {
 
@@ -21,10 +24,14 @@ public class EntityMounter extends Mounter {
     }
 
     protected void handle(Action event) {
-        Entity entity;
+
+        Entity dest = box.graph().getEntity(event.destEntity());
+        changeAttribute(dest, event.destEntityAttribute(), event.value(), event.operationType());
+        dest.save$();
     }
 
     protected void handle(AttachEntity event) {
+
         Entity parent = box.graph().getEntity(event.parent());
         Entity child = box.graph().getEntity(event.child());
 
@@ -46,11 +53,17 @@ public class EntityMounter extends Mounter {
     }
 
     protected void handle(DestroyEntity event) {
-        box.graph().entityList(e -> e.id().equals(event.id()))
-                .findFirst().ifPresent(Layer::delete$);
+
+        Entity entity = box.graph().getEntity(event.id());
+
+        if(entity == null) return;
+
+        entity.children().forEach(c -> c.parent(null).save$());
+        entity.delete$();
     }
 
     protected void handle(DetachEntity event) {
+
         Entity parent = box.graph().getEntity(event.parent());
         Entity child = box.graph().getEntity(event.child());
 
@@ -66,5 +79,25 @@ public class EntityMounter extends Mounter {
 
     protected void handle(CreateEntity event) {
         box.graph().entity(event).save$();
+    }
+
+    private void changeAttribute(Entity entity, String key, String value, Action.OperationType type) {
+
+        Map<String, String> attributes = new Gson().fromJson(entity.attributes(), new TypeToken<Map<String, String>>(){}.getType());
+
+        switch (type) {
+            case Add:
+                break;
+            case Remove:
+                break;
+            case Replace:
+                break;
+            case Sum:
+                break;
+            case Subtract:
+                break;
+        }
+
+        attributes.put(key, value);
     }
 }

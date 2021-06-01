e3c44558c07544d423d5be9ac4e8a23f1195449f
RaulLozanoPonce <rlozano@monentia.es>
2021-06-01 11:03:19 +0100

    Add terminal api in Engine class

--- core/src/io/intino/gamification/Engine.java
+++ core/src/io/intino/gamification/Engine.java
@@ -1,11 +1,14 @@
 package io.intino.gamification;
 
 import io.intino.alexandria.core.BoxConfiguration;
+import io.intino.gamification.core.box.CoreBox;
 import io.intino.gamification.core.box.launcher.Launcher;
+import io.intino.gamification.core.box.terminal.Terminal;
 
 public class Engine {
 
     private final Launcher launcher;
+    private CoreBox box;
 
     public Engine(BoxConfiguration configuration) {
         this.launcher = new Launcher(configuration);
@@ -13,10 +16,15 @@ public class Engine {
 
     public void launch() {
         launcher.start();
+        box = launcher.box();
     }
 
     public void launch(Runnable onStartCallback) {
         launcher.onStart(onStartCallback);
         launch();
+    }
+
+    public Terminal terminal() {
+        return box.terminal();
     }
 }

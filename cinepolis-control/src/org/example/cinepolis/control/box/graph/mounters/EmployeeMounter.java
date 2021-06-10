c69db7bb3ae035131273088d6a63286432810ef7
RaulLozanoPonce <rlozano@monentia.es>
2021-06-10 09:49:15 +0100

    Cinepolis example mounters

--- cinepolis-control/src/org/example/cinepolis/control/box/graph/mounters/EmployeeMounter.java
+++ cinepolis-control/src/org/example/cinepolis/control/box/graph/mounters/EmployeeMounter.java
@@ -1,11 +1,12 @@
 package org.example.cinepolis.control.box.graph.mounters;
 
-import org.example.cinepolis.control.box.ControlBox;
 import io.intino.alexandria.event.Event;
-
+import org.example.cinepolis.control.box.ControlBox;
 import org.example.cinepolis.control.box.mounters.Mounter;
+import org.example.cinepolis.control.graph.Employee;
 
 public class EmployeeMounter implements Mounter {
+
 	private final ControlBox box;
 
 	public EmployeeMounter(ControlBox box) {
@@ -13,11 +14,15 @@ public class EmployeeMounter implements Mounter {
 	}
 
 	public void handle(org.example.cinepolis.datahub.events.cinepolis.HireEmployee event) {
-
+		Employee employee = box.graph().employee(event.id());
+		if(employee != null) return;
+		box.graph().create("Employees").employee(event.id(), event.name(), event.age(), event.phone());
 	}
 
 	public void handle(org.example.cinepolis.datahub.events.cinepolis.DismissEmployee event) {
-
+		Employee employee = box.graph().employee(event.id());
+		if(employee == null) return;
+		employee.delete$();
 	}
 
 	public void handle(Event event) {
--- cinepolis-control/src/org/example/cinepolis/control/graph/ControlGraph.java
+++ cinepolis-control/src/org/example/cinepolis/control/graph/ControlGraph.java
@@ -2,6 +2,8 @@ package org.example.cinepolis.control.graph;
 
 import io.intino.magritte.framework.Graph;
 
+import java.util.List;
+
 public class ControlGraph extends org.example.cinepolis.control.graph.AbstractGraph {
 
 	public ControlGraph(Graph graph) {
@@ -10,5 +12,17 @@ public class ControlGraph extends org.example.cinepolis.control.graph.AbstractGr
 
 	public ControlGraph(io.intino.magritte.framework.Graph graph, ControlGraph wrapper) {
 	    super(graph, wrapper);
+	}
+
+	public Asset asset(String id) {
+		return assetList(a -> a.id().equals(id)).findFirst().orElse(null);
+	}
+
+	public Alert alert(List<Alert> alerts, String id) {
+		return alerts.stream().filter(a -> a.id().equals(id)).findFirst().orElse(null);
+	}
+
+	public Employee employee(String id) {
+		return employeeList(a -> a.id().equals(id)).findFirst().orElse(null);
 	}
 }

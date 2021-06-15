ea16a329eda04bad324725d82ada35a0f35692e9
RaulLozanoPonce <rlozano@monentia.es>
2021-06-15 13:36:06 +0100

    New Mission progress status

---
+++ core/src/io/intino/gamification/core/box/logic/CheckResult.java
@@ -0,0 +1,5 @@
+package io.intino.gamification.core.box.logic;
+
+public enum CheckResult {
+    Progress, Skip, Cancel
+}
--- core/src/io/intino/gamification/core/box/logic/CheckerHandler.java
+++ core/src/io/intino/gamification/core/box/logic/CheckerHandler.java
@@ -14,22 +14,22 @@ public class CheckerHandler {
     private static final Map<String, Checker<? extends GamificationEvent>> MissionCheckerMap = new HashMap<>();
 
     @SuppressWarnings("unchecked")
-    public static <T extends GamificationEvent> AchievementProgress check(Achievement achievement, T event, Player player) {
+    public static <T extends GamificationEvent> CheckResult check(Achievement achievement, T event, Player player) {
         try {
             Checker<T> checker = (Checker<T>) AchievementCheckerMap.get(achievement.id());
-            return checker.checkAchievement(event, player);
+            return checker.check(event, player);
         } catch(ClassCastException e) {
-            return AchievementProgress.NotProgress;
+            return CheckResult.Skip;
         }
     }
 
     @SuppressWarnings("unchecked")
-    public static <T extends GamificationEvent> boolean check(Mission mission, T event, Player player) {
+    public static <T extends GamificationEvent> CheckResult check(Mission mission, T event, Player player) {
         try {
             Checker<T> checker = (Checker<T>) MissionCheckerMap.get(mission.id());
-            return checker.checkMission(event, player);
+            return checker.check(event, player);
         } catch(ClassCastException e) {
-            return false;
+            return CheckResult.Skip;
         }
     }
 
@@ -42,11 +42,6 @@ public class CheckerHandler {
     }
 
     public interface Checker<T> {
-        AchievementProgress checkAchievement(T event, Player player);
-        boolean checkMission(T event, Player player);
-    }
-
-    public enum AchievementProgress {
-        Progress, NotProgress, Cancel
+        CheckResult check(T event, Player player);
     }
 }

835189041710e786603a57adb77ebed73701d028
RaulLozanoPonce <rlozano@monentia.es>
2021-07-30 13:36:53 +0100

    Utils module

--- core/test/io/intino/gamification/test/Test.java
+++ core/test/io/intino/gamification/test/Test.java
@@ -2,17 +2,21 @@ package io.intino.gamification.test;
 
 import io.intino.gamification.GamificationEngine;
 
+import java.time.temporal.ChronoUnit;
+
 import static io.intino.gamification.core.box.utils.TimeUtils.Scale.Hour;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        GamificationEngine engine = null;
+        String instant1 = "2021-07-30T13:29:45.32";
+        String instant2 = "2021-07-30T13:29:45";
 
-        //engine.configuration().healthListener.set((entity, oldValue, newValue) -> oldValue);
-
-        engine.configuration().gameLoopConfigurator.schedule(1, Hour);
+        System.out.println(String.valueOf((int) Float.parseFloat(instant1.split("[TZ]")[1].split(":")[2])));
+        System.out.println(String.valueOf((int) Float.parseFloat(instant2.split("[TZ]")[1].split(":")[2])));
+        System.out.println(String.valueOf((int) (1000 * (Float.parseFloat(instant1.split("[TZ]")[1].split(":")[2]) % 1))));
+        System.out.println(String.valueOf((int) (1000 * (Float.parseFloat(instant2.split("[TZ]")[1].split(":")[2]) % 1))));
     }
 }
 
---
+++ events/src/io/intino/gamification/events/EventSealer.java
@@ -0,0 +1,8 @@
+package io.intino.gamification.events;
+
+import java.io.File;
+
+public class EventSealer {
+
+    private File
+}

75ea8c39458b488c89bb71332edd23a21ddd681b
cherrera <naitsirc98@gmail.com>
2021-09-27 12:53:56 +0100

    Refactor Missions_ test

--- core/test/io/intino/gamification/test/util/EngineTestHelper.java
+++ core/test/io/intino/gamification/test/util/EngineTestHelper.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class EngineUtils_ {
+public class EngineTestHelper {
 
     public static GamificationEngine getEngine() {
 

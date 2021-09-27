ebfd5b83789c953679da10981a0bd48c167fb3c2
cherrera <naitsirc98@gmail.com>
2021-09-27 12:53:56 +0100

    Refactor Missions_ test

--- core/test/io/intino/gamification/test/Missions_.java
+++ core/test/io/intino/gamification/test/Missions_.java
@@ -3,8 +3,9 @@ package io.intino.gamification.test;
 import io.intino.gamification.GamificationEngine;
 import io.intino.gamification.graph.GamificationGraph;
 import io.intino.gamification.graph.model.Match;
-import io.intino.gamification.test.util.EngineUtils_;
+import io.intino.gamification.test.util.EngineTestHelper;
 import io.intino.gamification.test.util.events.FixAsset;
+import io.intino.gamification.test.util.model.Asset;
 import io.intino.gamification.test.util.model.Cinesa;
 import io.intino.gamification.test.util.model.FixFiveAsset;
 import io.intino.gamification.test.util.model.Workday;
@@ -25,17 +26,19 @@ import static org.junit.Assert.assertEquals;
 @RunWith(org.junit.runners.Parameterized.class)
 public class Missions_ {
 
+    private static final float EPSILON = 0.00001f;
+
     private static GamificationEngine engine;
     private static Cinesa world;
 
-    private final Integer nFixAsset;
-    private final Boolean finishMatch;
+    private final int nFixAsset;
+    private final boolean finishMatch;
     private final Action action;
-    private final Integer score;
-    private final Float progress;
+    private final int score;
+    private final float progress;
     private final State state;
 
-    public Missions_(Integer nFixAsset, Boolean finishMatch, Missions_.Action action, Integer score, Float progress, Progress.State state) {
+    public Missions_(int nFixAsset, boolean finishMatch, Missions_.Action action, int score, float progress, Progress.State state) {
         this.nFixAsset = nFixAsset;
         this.finishMatch = finishMatch;
         this.action = action;
@@ -53,11 +56,11 @@ public class Missions_ {
                 {0, true, null, DO_NOTHING_SCORE, 0f, Progress.State.Failed},
                 {3, true, null, 60, 0.6f, Progress.State.Failed},
                 {5, true, null, COMPLETE_SCORE, 1f, Progress.State.Complete},
-                {0, false, Missions_.Action.Cancel, 0, null, null},
-                {3, false, Missions_.Action.Cancel, 0, null, null},
+                {0, false, Missions_.Action.Cancel, 0, 0, null},
+                {3, false, Missions_.Action.Cancel, 0, 0, null},
                 {5, false, Missions_.Action.Cancel, COMPLETE_SCORE, 1f, Progress.State.Complete},
-                {0, true, Missions_.Action.Cancel, 0, null, null},
-                {3, true, Missions_.Action.Cancel, 0, null, null},
+                {0, true, Missions_.Action.Cancel, 0, 0, null},
+                {3, true, Missions_.Action.Cancel, 0, 0, null},
                 {5, true, Missions_.Action.Cancel, COMPLETE_SCORE, 1f, Progress.State.Complete},
                 {0, false, Missions_.Action.Fail, FAIL_SCORE, 0f, Progress.State.Failed},
                 {3, false, Missions_.Action.Fail, FAIL_SCORE, 0.6f, Progress.State.Failed},
@@ -76,7 +79,7 @@ public class Missions_ {
 
     @Before
     public void before() {
-        engine = EngineUtils_.getEngine();
+        engine = EngineTestHelper.getEngine();
         GamificationGraph graph = engine.graph();
         world = new Cinesa("world");
         graph.createWorld(world);
@@ -84,7 +87,7 @@ public class Missions_ {
         FixFiveAsset mission = new FixFiveAsset();
         world.missions().add(mission);
 
-        EngineUtils_.initTechnician(world, "t1", Arrays.asList("a1", "a2"));
+        EngineTestHelper.initTechnician(world, "t1", Arrays.asList("a1", "a2"));
 
         world.startNewMatch(new Workday("world", "match"));
 
@@ -92,12 +95,77 @@ public class Missions_ {
     }
 
     @Test
-    public void execute(){
+    public void execute() {
+       try {
+
+           Match.PlayerState playerState = setup();
+           checkMissionAssignment(playerState);
+           assertFinalScoreMatchesExpectedScore(playerState);
+
+       } catch (AssertionError e) {
+           AssertionError error = new AssertionError("Test " + this + " failed: " + e.getMessage());
+           error.setStackTrace(e.getStackTrace());
+           throw error;
+       }
+    }
+
+    private void assertFinalScoreMatchesExpectedScore(Match.PlayerState playerState) {
+        assertEquals(score, playerState.score());
+    }
+
+    private void checkMissionAssignment(Match.PlayerState playerState) {
+
+        if(action == Action.Cancel && state != Progress.State.Complete) {
+            assertThereAreNoMissionAssignmentsLeft(playerState);
+        } else {
+            assertMissionAssignmentIsStillPresent(playerState);
+        }
+
+        if(action != Missions_.Action.Cancel || state == Progress.State.Complete) {
+            assertProgressMatchesExpectedProgress(playerState);
+            assertFinalStateMatchesExpected(playerState);
+        }
+    }
+
+    private void assertThereAreNoMissionAssignmentsLeft(Match.PlayerState playerState) {
+        assertEquals(0, playerState.missionAssignments().size());
+    }
+
+    private void assertMissionAssignmentIsStillPresent(Match.PlayerState playerState) {
+        assertEquals(1, playerState.missionAssignments().size());
+    }
+
+    private void assertProgressMatchesExpectedProgress(Match.PlayerState playerState) {
+        assertEquals(progress, playerState.missionAssignments().get(0).progress().get(), EPSILON);
+    }
+
+    private void assertFinalStateMatchesExpected(Match.PlayerState playerState) {
+        assertEquals(state, playerState.missionAssignments().get(0).progress().state());
+    }
+
+    private Match.PlayerState setup() {
 
         for (int i = 0; i < nFixAsset; i++) {
             engine.eventPublisher().publish(new FixAsset(world.id(), "t1"));
         }
 
+        runAction();
+
+        return getFinalPlayerState();
+    }
+
+    private Match.PlayerState getFinalPlayerState() {
+        Match.PlayerState playerState;
+        if(finishMatch) {
+            world.finishCurrentMatch();
+            playerState = world.finishedMatches().find("match").players().get("t1");
+        } else {
+            playerState = world.currentMatch().players().get("t1");
+        }
+        return playerState;
+    }
+
+    private void runAction() {
         if(action != null) {
             switch (action) {
                 case Cancel:
@@ -111,31 +179,21 @@ public class Missions_ {
                     break;
             }
         }
-
-        Match.PlayerState playerState;
-
-        if(finishMatch) {
-            world.finishCurrentMatch();
-            playerState = world.finishedMatches().find("match").players().get("t1");
-        } else {
-            playerState = world.currentMatch().players().get("t1");
-        }
-
-        if(action == Missions_.Action.Cancel && state != Progress.State.Complete) {
-            assertEquals(0, playerState.missionAssignments().size());
-        } else {
-            assertEquals(1, playerState.missionAssignments().size());
-        }
-
-        assertEquals(score.intValue(), playerState.score());
-
-        if(action != Missions_.Action.Cancel || state == Progress.State.Complete) {
-            assertEquals(progress, playerState.missionAssignments().get(0).progress().get(), 0.00001f);
-            assertEquals(state, playerState.missionAssignments().get(0).progress().state());
-        }
     }
 
     public enum Action {
         Cancel, Complete, Fail
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "nFixAsset=" + nFixAsset +
+                ", finishMatch=" + finishMatch +
+                ", action=" + action +
+                ", score=" + score +
+                ", progress=" + progress +
+                ", state=" + state +
+                '}';
     }
 }
